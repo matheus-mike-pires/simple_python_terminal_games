d72c9bc6720f2b969c914649cd71b82aa8cf3903
matheus-mike-pires <matheusolv.pires@gmail.com>
2026-08-28 13:29:05 -0300

    Update HangmainV1.py
    Extra instructions

--- individual_games/hangman_game/HangmainV1.py
+++ individual_games/hangman_game/HangmainV1.py
@@ -1,4 +1,4 @@
-#To play the hangman game, keep the hangman_words in the same directory. 'from hangman_words import words' should do the job. Also, feel free to add words to the hangman_words
+#To play the hangman game, keep the hangman_words.py in the same directory as HangmainV1.py. 'from hangman_words import words' should do the job. Also, feel free to add words to the hangman_words
 
 import random as rd
 
